04bfe836eff447d3e553a4e5923df6c6b5a950a8
Duna Jilang <jsduna@gmail.com>
2020-05-17 05:18:32 +0100

    Just some UI with bottom navigation bar

---
+++ lib/pages/HomePage.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isSignedIn = false;
+  Widget buildHomeScreen() {
+    return Text("Already SignIn");
+  }
+
+  //build signIn Screen
+  Scaffold buildSignInScreen() {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (isSignedIn) {
+      return buildHomeScreen();
+    } else {
+      return buildSignInScreen();
+    }
+  }
+}
